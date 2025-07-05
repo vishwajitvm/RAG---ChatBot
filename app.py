90d24dde4a469efee0a8e9b0e1afe4543302b6db
vishwait <vishwajeet.mall@bee-logical.com>
2025-07-05 21:44:37 +0530

    Reorganize import statements for consistency and clarity

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-import streamlit as st
-from PyPDF2 import PdfReader
-import pandas as pd
-import base64
-import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
@@ -11,6 +6,11 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from datetime import datetime
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+import streamlit as st
+import pandas as pd
+import base64
+import os
 load_dotenv()
 
 def get_pdf_text(pdf_docs):
